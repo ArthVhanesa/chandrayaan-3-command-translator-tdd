3296758c358e736c28333e864321dcd5f8927e5a
Arth Vhanesa <77712031+ArthVhanesa@users.noreply.github.com>
2023-08-30 00:23:57 +0530

    test add for object

--- tests/spacecraft.test.js
+++ tests/spacecraft.test.js
@@ -1,7 +1,20 @@
 const { Spacecraft } = require("../src/spacecraft");
 
-test("Initialize spacecraft with values", () => {
-  const spacecraft = new Spacecraft(0, 0, 0, "N");
+describe("Basic initialization testing", () => {
+  test("Initialize spacecraft with values", () => {
+    const spacecraft = new Spacecraft(0, 0, 0, "N");
 
-  expect(spacecraft).toBeDefined();
+    expect(spacecraft).toBeDefined();
+  });
+
+  test("Initialize spacecraft with values", () => {
+    const spacecraft = new Spacecraft(0, 0, 0, "N");
+
+    expect(spacecraft).toEqual({
+      x: 0,
+      y: 0,
+      z: 0,
+      direction: "N",
+    });
+  });
 });
