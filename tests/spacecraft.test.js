a8f3c87a3e8d02f5adb452ebc62ccd7d778b699f
Arth Vhanesa <77712031+ArthVhanesa@users.noreply.github.com>
2023-08-30 00:36:15 +0530

    + add forward movement test

--- tests/spacecraft.test.js
+++ tests/spacecraft.test.js
@@ -1,7 +1,7 @@
 const { Spacecraft } = require("../src/spacecraft");
 
 describe("Basic initialization testing", () => {
-  test("Initialize spacecraft with values", () => {
+  test("Initialize spacecraft", () => {
     const spacecraft = new Spacecraft(0, 0, 0, "N");
 
     expect(spacecraft).toBeDefined();
@@ -13,6 +13,20 @@ describe("Basic initialization testing", () => {
     expect(spacecraft).toEqual({
       x: 0,
       y: 0,
+      z: 0,
+      direction: "N",
+    });
+  });
+});
+
+describe("Forward movement testing", () => {
+  test("Move forward with North", () => {
+    const spacecraft = new Spacecraft(0, 0, 0, "N");
+    spacecraft.moveForward();
+
+    expect(spacecraft).toEqual({
+      x: 0,
+      y: 1,
       z: 0,
       direction: "N",
     });
