f65d82d1e3faf86b966db2c3f103888e10e7835b
Arth Vhanesa <77712031+ArthVhanesa@users.noreply.github.com>
2023-08-30 00:42:59 +0530

    + add test case for forward with all direction

--- tests/spacecraft.test.js
+++ tests/spacecraft.test.js
@@ -31,4 +31,52 @@ describe("Forward movement testing", () => {
       direction: "N",
     });
   });
+
+  test("Move forward with South", () => {
+    const spacecraft = new Spacecraft(0, 0, 0, "S");
+    spacecraft.moveForward();
+
+    expect(spacecraft).toEqual({
+      x: 0,
+      y: -1,
+      z: 0,
+      direction: "S",
+    });
+  });
+
+  test("Move forward with East", () => {
+    const spacecraft = new Spacecraft(0, 0, 0, "E");
+    spacecraft.moveForward();
+
+    expect(spacecraft).toEqual({
+      x: 1,
+      y: 0,
+      z: 0,
+      direction: "E",
+    });
+  });
+
+  test("Move forward with Up", () => {
+    const spacecraft = new Spacecraft(0, 0, 0, "U");
+    spacecraft.moveForward();
+
+    expect(spacecraft).toEqual({
+      x: 0,
+      y: 0,
+      z: 1,
+      direction: "U",
+    });
+  });
+
+  test("Move forward with Down", () => {
+    const spacecraft = new Spacecraft(0, 0, 0, "D");
+    spacecraft.moveForward();
+
+    expect(spacecraft).toEqual({
+      x: 0,
+      y: 0,
+      z: -1,
+      direction: "D",
+    });
+  });
 });
