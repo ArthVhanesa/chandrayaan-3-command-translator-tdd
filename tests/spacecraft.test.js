c579cd6b28247c438202849e9abf533009d275d8
Arth Vhanesa <77712031+ArthVhanesa@users.noreply.github.com>
2023-08-30 01:17:13 +0530

    + add test - turn left with all direction

--- tests/spacecraft.test.js
+++ tests/spacecraft.test.js
@@ -179,4 +179,64 @@ describe("Turn left testing", () => {
       direction: "W",
     });
   });
+
+  test("Move left with South", () => {
+    const spacecraft = new Spacecraft(0, 0, 0, "S");
+    spacecraft.turnLeft();
+
+    expect(spacecraft).toEqual({
+      x: 0,
+      y: 0,
+      z: 0,
+      direction: "E",
+    });
+  });
+
+  test("Move left with East", () => {
+    const spacecraft = new Spacecraft(0, 0, 0, "E");
+    spacecraft.turnLeft();
+
+    expect(spacecraft).toEqual({
+      x: 0,
+      y: 0,
+      z: 0,
+      direction: "N",
+    });
+  });
+
+  test("Move left with West", () => {
+    const spacecraft = new Spacecraft(0, 0, 0, "W");
+    spacecraft.turnLeft();
+
+    expect(spacecraft).toEqual({
+      x: 0,
+      y: 0,
+      z: 0,
+      direction: "S",
+    });
+  });
+
+  test("Move left with Up", () => {
+    const spacecraft = new Spacecraft(0, 0, 0, "U");
+    spacecraft.turnLeft();
+
+    expect(spacecraft).toEqual({
+      x: 0,
+      y: 0,
+      z: 0,
+      direction: "N",
+    });
+  });
+
+  test("Move left with Down", () => {
+    const spacecraft = new Spacecraft(0, 0, 0, "D");
+    spacecraft.turnLeft();
+
+    expect(spacecraft).toEqual({
+      x: 0,
+      y: 0,
+      z: 0,
+      direction: "S",
+    });
+  });
 });
