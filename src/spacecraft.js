c446efdcfb08013995ea2cb56f4904fa990b0242
Arth Vhanesa <77712031+ArthVhanesa@users.noreply.github.com>
2023-08-30 01:25:00 +0530

    pass test - turn left with all directions

--- src/spacecraft.js
+++ src/spacecraft.js
@@ -80,6 +80,31 @@ class Spacecraft {
         break;
     }
   }
+
+  turnRight() {
+    switch (this.direction) {
+      case "N":
+        this.direction = "E";
+        break;
+      case "S":
+        this.direction = "W";
+        break;
+      case "E":
+        this.direction = "S";
+        break;
+      case "W":
+        this.direction = "N";
+        break;
+      case "U":
+        this.direction = "S";
+        break;
+      case "D":
+        this.direction = "N";
+        break;
+      default:
+        break;
+    }
+  }
 }
 
 module.exports = { Spacecraft };
